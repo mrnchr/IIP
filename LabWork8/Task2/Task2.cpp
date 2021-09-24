63da7b4e82fbf377b5645bba1fdc111d7230bdd5
Subbota Mikhail <mihail.subbota@inbox.ru>
2021-09-24 22:53:45 +0300

    LabWork 8

---
+++ LabWork8/Task2/Task2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cout << "Enter A and B separated by size: ";
+	cin >> a >> b;
+
+	cout << "A consists of " << a / b << " lenghts B" << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ LabWork8/Task3/Task3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cout << "Enter A and B: ";
+	cin >> a >> b;
+
+	cout << "Answer: " << a % b << endl;
+
+	system("pause");
+	return 0;
+}
