3aeba4271c636b282f3ae72a04b9e68245def81a
Subbota Mikhail <mihail.subbota@inbox.ru>
2021-09-25 20:20:52 +0300

    LabWork 9 version 2

--- LabWork9/Task5/Task5.cpp
+++ LabWork9/Task5/Task5.cpp
@@ -12,11 +12,7 @@ int main() {
 
 	cout << "This is " << N;
 
-<<<<<<< HEAD
 	//Output of the end depending on the century
-=======
-	//Вывод окончания в зависимости от века
->>>>>>> 4a67d110958ed2bfcd5dbdae7ea6d7fc427e21cd
 	if (N / 10 != 1) {
 		switch (N % 10) {
 		case 1: cout << "st";
