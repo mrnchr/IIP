3bfb4ec0529a6047bb84eefe390b1bb7b8b11cff
Subbota Mikhail <mihail.subbota@inbox.ru>
2021-10-05 11:18:14 +0300

    LabWork 13

--- LabWork11/Task5/Task5.cpp
+++ LabWork11/Task5/Task5.cpp
@@ -1,25 +1,27 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "Russian");
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
 
-	int a;
-	string sign, even;
+int a;
+string sign, even;
 
-	cout << "Enter the number: ";
-	cin >> a;
+cout << "Enter the number: ";
+cin >> a;
 
-	if (a % 2) even = "нечетное";
-	else even = "четное";
+if (a % 2) even = "нечетное";
+else even = "четное";
 
-	if (a < 0) sign = "отрицательное";
-	else if (a > 0) sign = "положительное";
+if (a < 0) sign = "отрицательное";
+else if (a > 0) sign = "положительное";
 
-	if (a == 0) cout << "нулевое число" << endl;
-	else cout << sign << " " << even << " " << "число" << endl;
+if (a == 0) cout << "нулевое число" << endl;
+else cout << sign << " " << even << " " << "число" << endl;
 
-	system("pause");
-	return 0;
+system("pause");
+return 0;
 }
